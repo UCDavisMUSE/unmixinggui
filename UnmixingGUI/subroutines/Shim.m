469c374ed1608ab707127ffc3c14a2c295d39df5
Zachary T. Harmany <zharmany@gmail.com>
2016-11-01 12:13:24 -0700

    Added feature to scale components such that the maximum value is white.

--- UnmixingGUI/subroutines/Shim.m
+++ UnmixingGUI/subroutines/Shim.m
@@ -1,6 +1,11 @@
 function Shim(X, n, N, myTitle)
 Pos(n, N)
-imagesc(X);
+scaleMaxIntensity = 1;
+if scaleMaxIntensity
+    imagesc(X,[0, max(X(:))]);
+else
+    imagesc(X);
+end
 axis image off;
 colormap(gray);
 title(myTitle)
